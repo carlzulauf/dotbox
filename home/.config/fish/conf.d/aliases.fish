186ed2ffb46bc907a519c5ea30517101f80b3621
Carl Zulauf <carl@linkleaf.com>
2022-11-06 22:34:56 -0700

    Suppress `which` errors on `exa` lookup

--- home/.config/fish/conf.d/aliases.fish
+++ home/.config/fish/conf.d/aliases.fish
@@ -5,7 +5,7 @@ alias fix_keychron='echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode'
 alias strip_colors='sed \'s/\x1B\[[0-9;]\{1,\}[A-Za-z]//g\''
 
 # Replace ls with exa if available
-if which exa > /dev/null
+if which exa &> /dev/null
   source (starship init fish --print-full-init | psub)
   alias l='exa -a --color=always --group-directories-first --icons'   # short, everything
   alias ls='exa --color=always --group-directories-first --icons'     # preferred listing
