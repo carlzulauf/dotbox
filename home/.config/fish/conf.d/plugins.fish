7be6ef707630e0a13dc164c90620ff994768c649
Carl Zulauf <carl@linkleaf.com>
2023-02-04 18:35:00 -0700

    Provide alternative/fallback for starship prompt in fish

--- home/.config/fish/conf.d/plugins.fish
+++ home/.config/fish/conf.d/plugins.fish
@@ -1,6 +1,19 @@
 ## Starship prompt if we're interactive and starship in PATH
-if status --is-interactive && type -q starship
-  source (starship init fish --print-full-init | psub)
+
+# If we're interactive, configure the prompt
+if status --is-interactive
+
+  # use starship prompt if it's available
+  if type -q starship
+    source (starship init fish --print-full-init | psub)
+
+  else # otherwise, look in fish_prompt.fish which uses terlar prompt by default
+
+    if test -f ~/.config/fish/fish_prompt.fish
+      source ~/.config/fish/fish_prompt.fish
+    end
+
+  end
 end
 
 ## Advanced command-not-found hook, if found
