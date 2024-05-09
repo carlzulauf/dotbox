65b195798a83b9fa8bb689f367260100c5fdc938
Carl Zulauf <carl@unabridgedsoftware.com>
2024-05-09 09:23:50 -0600

    Switch fish greeting to use fastfetch instead of neofetch

--- home/.config/fish/functions/fish_greeting.fish
+++ home/.config/fish/functions/fish_greeting.fish
@@ -1,8 +1,19 @@
 function fish_greeting
-  if type -q neofetch
+  if type -q fastfetch
     # if it's not set up, setup the global with a value that will show MOTD
-    # Note: if you are time traveling prior to the unix epoch you will need to
-    #       initialize this variable with a sufficiently negative value.
+    if test -z "$MOTD_LAST_SHOWN_AT"
+      set --universal MOTD_LAST_SHOWN_AT 1
+    end
+
+    # show fastfetch if we haven't shown it for 2hrs
+    if test (date +%s) -gt (math $MOTD_LAST_SHOWN_AT + 7200)
+      set --universal MOTD_LAST_SHOWN_AT (date +%s)
+      fastfetch
+    else
+      date
+    end
+  else if type -q neofetch
+    # if it's not set up, setup the global with a value that will show MOTD
     if test -z "$MOTD_LAST_SHOWN_AT"
       set --universal MOTD_LAST_SHOWN_AT 1
     end
