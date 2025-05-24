c9f9c19649aa65ac881bd9810c57b4139b5ecbeb
Carl Zulauf <carl@unabridgedsoftware.com>
2025-05-24 03:22:16 -0600

    Disable asdf outside of containers

--- home/.config/fish/conf.d/paths.fish
+++ home/.config/fish/conf.d/paths.fish
@@ -1,4 +1,11 @@
-# Add ~/.local/bin to PATH, early (prepend)
+# Add ~/.asdf/shims to PATH early (prepend), but only in containers
+if test ~/.asdf/shims; and test -n "$CONTAINER_ID"
+  if not contains -- ~/.asdf/shims $PATH
+    set --prepend PATH ~/.asdf/shims
+  end
+end
+
+# Add ~/.local/bin to PATH early (prepend) before asdf
 if test -d ~/.local/bin
   if not contains -- ~/.local/bin $PATH
     set --prepend PATH ~/.local/bin
@@ -6,15 +13,8 @@ if test -d ~/.local/bin
 end
 
 # Add gem bins to PATH, late (append)
-if test -d ~/.local/share/gem/ruby/3.0.0/bin
-  if not contains -- ~/.local/share/gem/ruby/3.0.0/bin $PATH
-    set --append PATH ~/.local/share/gem/ruby/3.0.0/bin
-  end
-end
-
-# Add gem bins to PATH, late (append)
-if test -d ~/.gem/ruby/3.0.0/bin
-  if not contains -- ~/.gem/ruby/3.0.0/bin $PATH
-    set --append PATH ~/.gem/ruby/3.0.0/bin
-  end
-end
+# if test -d ~/.gem/ruby/3.0.0/bin
+#   if not contains -- ~/.gem/ruby/3.0.0/bin $PATH
+#     set --append PATH ~/.gem/ruby/3.0.0/bin
+#   end
+# end
