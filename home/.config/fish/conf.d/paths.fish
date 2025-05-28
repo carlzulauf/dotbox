8ed5ae7f626aebc0364b3cdfbc224d082f1ca4a6
Carl Zulauf <carl@unabridgedsoftware.com>
2025-05-28 12:35:34 -0600

    Add asdf shims only if asdf is a command

--- home/.config/fish/conf.d/paths.fish
+++ home/.config/fish/conf.d/paths.fish
@@ -1,13 +1,11 @@
-if test -d ~/.asdf/shims
-  # if we're not on NixOS, or we're in a container, then add asdf to PATH
-  if string match -eqv 'NixOS' (uname -a); or test -n "$CONTAINER_ID"
-    if not contains -- ~/.asdf/shims $PATH
-      set --prepend PATH ~/.asdf/shims
-    end
+# if shims are present and asdf is loaded, make sure shims are early in PATH
+if test -d ~/.asdf/shims; and type -q asdf
+  if not contains -- ~/.asdf/shims $PATH
+    set --prepend PATH ~/.asdf/shims
   end
 end
 
-# Add ~/.local/bin to PATH early (prepend) before asdf
+# Add ~/.local/bin to PATH early, even before asdf
 if test -d ~/.local/bin
   if not contains -- ~/.local/bin $PATH
     set --prepend PATH ~/.local/bin
