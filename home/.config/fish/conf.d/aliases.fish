44869d6142f8656d795c75765b3b33ca33a5d60f
Carl Zulauf <carl@unabridgedsoftware.com>
2025-11-20 17:30:20 -0600

    lsfs makes more sense than fsls, but we can have both

--- home/.config/fish/conf.d/aliases.fish
+++ home/.config/fish/conf.d/aliases.fish
@@ -6,8 +6,8 @@ alias strip_colors='sed \'s/\x1B\[[0-9;]\{1,\}[A-Za-z]//g\''
 alias dc='docker-compose'
 alias dcr='docker-compose run --rm -it'
 
-# show all btrfs and ext4 mount points, plus any merged filesystems, and boot
-alias fsls="dysk -a -f 'type=btrfs|type=ext4|type=merger|type=vfat|type=f2fs|type=ntfs'"
+alias lsfs="dysk -a -f 'type=btrfs|type=ext4|type=merger|type=vfat|type=f2fs|type=ntfs'"
+alias fsls=lsfs
 
 # Replace ls with eza if available
 if type -q eza
