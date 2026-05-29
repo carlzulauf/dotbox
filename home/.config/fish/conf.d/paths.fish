3ebe2db7bbb95995637b64818edb0e344bf97184
Carl Zulauf <carl@unabridgedsoftware.com>
2026-05-29 09:16:51 -0500

    Cleanup fish paths a bit

--- home/.config/fish/conf.d/paths.fish
+++ home/.config/fish/conf.d/paths.fish
@@ -1,6 +1,30 @@
-if is_local_or_asdf_container
+function remove_nix_paths
+  if test -n "$NIX_GEM_BIN"
+    strip_path "$NIX_GEM_BIN"
+  end
+  strip_path "/nix/store/"
+  strip_path "nix*profile"
+end
+
+function remove_container_paths
+  if test -n "$CONTAINER_GEM_BIN"
+    strip_path "$CONTAINER_GEM_BIN"
+    strip_path "/.asdf/"
+  end
+end
+
+if is_container
+  remove_nix_paths
+
+  set --global --export CONTAINER_GEM_HOME "$HOME/.local/share/gems/$CONTAINER_ID"
+  set --global --export CONTAINER_GEM_BIN "$CONTAINER_GEM_HOME/bin"
+  set GEM_HOME $CONTAINER_GEM_HOME
+  set GEM_PATH $CONTAINER_GEM_HOME
+  if not contains $CONTAINER_GEM_BIN $PATH
+    set --prepend PATH $CONTAINER_GEM_BIN
+  end
+
   if type -q asdf
-    # if shims are present and asdf is loaded, make sure shims are early in PATH
     if test -d ~/.asdf/shims
       if not contains -- ~/.asdf/shims $PATH
         set --prepend PATH ~/.asdf/shims
@@ -9,42 +33,19 @@ if is_local_or_asdf_container
   else
     strip_path "/.asdf/"
   end
+else if type -q nix-store
+  remove_container_paths
 
-  if type -q nix-store
-    set --global --export NIX_GEM_HOME "$HOME/.local/share/gems/nix"
-    set --global --export NIX_GEM_BIN "$NIX_GEM_HOME/bin"
-
-    if test -n "$CONTAINER_GEM_BIN"
-      strip_path "$CONTAINER_GEM_BIN"
-    end
-
-    set GEM_HOME $NIX_GEM_HOME
-    set GEM_PATH $NIX_GEM_HOME
-    if not contains $NIX_GEM_BIN $PATH
-      set --append PATH $NIX_GEM_BIN
-    end
-  else
-    if test -n "$NIX_GEM_BIN"
-      strip_path "$NIX_GEM_BIN"
-    end
+  set --global --export NIX_GEM_HOME "$HOME/.local/share/gems/nix"
+  set --global --export NIX_GEM_BIN "$NIX_GEM_HOME/bin"
+  set GEM_HOME $NIX_GEM_HOME
+  set GEM_PATH $NIX_GEM_HOME
+  if not contains $NIX_GEM_BIN $PATH
+    set --prepend PATH $NIX_GEM_BIN
   end
-
 else
-  strip_path "/.asdf/"
-  if test -n "$NIX_GEM_BIN"
-    strip_path "$NIX_GEM_BIN"
-  end
-
-  if test -n "$CONTAINER_ID"
-    set --global --export CONTAINER_GEM_HOME "$HOME/.local/share/gems/$CONTAINER_ID"
-    set --global --export CONTAINER_GEM_BIN "$CONTAINER_GEM_HOME/bin"
-
-    set GEM_HOME $CONTAINER_GEM_HOME
-    set GEM_PATH $CONTAINER_GEM_HOME
-    if not contains $CONTAINER_GEM_BIN $PATH
-      set --append PATH $CONTAINER_GEM_BIN
-    end
-  end
+  remove_container_paths
+  remove_nix_paths
 end
 
 # Add ~/.local/bin to PATH early, even before asdf
